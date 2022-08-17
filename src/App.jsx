da6044347b1efba3e0872bf166b67c567b93c587
Dao Anh <daoanh9617@gmail.com>
2022-08-17 09:57:34 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<>OK</>}/>
+            <Route path="/" element={<>OK1</>}/>
         </Routes>
     </BrowserRouter>
   );
