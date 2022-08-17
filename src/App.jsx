b7878e2fd5c673393e56a4d6b8cbd722ace19807
Manh <lemanhct5b@gmail.com>
2022-08-17 15:00:18 +0700

    Nhanh cua Manh

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import 'antd/dist/antd.min.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AdminMenu from "./components/adminMenu/AdminMenu";
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<>OK</>}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<AdminMenu></AdminMenu>} />
+      </Routes>
     </BrowserRouter>
   );
 }
