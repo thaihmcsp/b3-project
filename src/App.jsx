fe5e141ad614b7f8ff0ebd193010bdf12d6cc979
thont <truongtho12092000@gmail.com>
2022-08-18 14:24:33 +0700

    tho

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Order from "./pages/admin/order/Order";
+import 'antd/dist/antd.css';
+import '../src/pages/admin/order/order.css'
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<>OK</>}/>
+            <Route path={'/pages/admin/order'} element={<Order/>}/>
         </Routes>
     </BrowserRouter>
   );
