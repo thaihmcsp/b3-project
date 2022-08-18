8d01c38b7b08a9cbe45fc7db0e6a7f3591116ed0
BuiCongMinh <65665940+BuiCongMinh@users.noreply.github.com>
2022-08-18 14:25:38 +0700

    Test_Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import 'antd/dist/antd.css'; 
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<>OK</>}/>
+            <Route path="/" element={<Footer/>}/>
         </Routes>
     </BrowserRouter>
   );
