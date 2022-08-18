d3e60a15b7ded3908f2f97d098be5a3a1117541a
truonghuygit <truonghuy20112005@gmail.com>
2022-08-18 12:51:22 +0700

    first commit

---
+++ src/components/FilterMenu.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { FiFilter } from "react-icons/fi";
+import { AiOutlineDown } from "react-icons/ai";
+import AppCss from "../App.css";
+
+function FilterMenu() {
+  return (
+    <div className="filter-menu">
+      <div className="filter-menu-header">
+        <FiFilter className="icon-filter" />
+        <h2>BỘ LỌC TÌM KIẾM</h2>
+      </div>
+
+      {/* ==============THEO DANH MỤC================= */}
+      <div className="filter-menu-list">
+        <p>Theo Danh Mục</p>
+
+        <div className="filter-item">
+          <input type="checkbox" />
+          <div>
+            <span>Áo thun (253k+)</span>
+          </div>
+        </div>
+
+        <div className="filter-item">
+          <input type="checkbox" />
+          <div>
+            <span>Thời Trang Trẻ Em (26+)</span>
+          </div>
+        </div>
+
+        <div className="filter-item">
+          <input type="checkbox" />
+          <div>
+            <span>Bộ (13+)</span>
+          </div>
+        </div>
+
+        <div className="add">
+          Thêm
+          <AiOutlineDown className="icon-add" />
+        </div>
+      </div>
+      <div className="line"></div>
+      {/* ==============THEO DANH MỤC (DONE)================= */}
+
+      {/* ==============THEO THƯƠNG HIỆU================= */}
+      <div className="filter-menu-list">
+        <p>Theo Thương Hiệu</p>
+
+        <div className="filter-item">
+          <input type="checkbox" />
+          <div>
+            <span>Logitech</span>
+          </div>
+        </div>
+
+        <div className="filter-item">
+          <input type="checkbox" />
+          <div>
+            <span>Yinxx</span>
+          </div>
+        </div>
+
+        <div className="filter-item">
+          <input type="checkbox" />
+          <div>
+            <span>2S Clothing</span>
+          </div>
+        </div>
+
+        <div className="add">
+          Thêm
+          <AiOutlineDown className="icon-add" />
+        </div>
+      </div>
+      <div className="line"></div>
+      {/* ==============THEO THƯƠNG HIỆU (DONE)================= */}
+
+      {/* ==============THEO KHOẢNG GIÁ================= */}
+      <div className="filter-menu-list">
+        <p>Khoảng Giá</p>
+
+        <div className="filter-item-price">
+          <input type="text" placeholder="₫ TỪ" className="price" />
+          <div className="price-line"></div>
+          <input type="text" placeholder="₫ ĐẾN" className="price" />
+        </div>
+        <button className="price-btn">ÁP DỤNG</button>
+      </div>
+    </div>
+  );
+}
+
+export default FilterMenu;
