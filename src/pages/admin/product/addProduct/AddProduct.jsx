cef77e6b3a637ca8f4e2b84f03134ecc7fd34968
thont <truongtho12092000@gmail.com>
2022-09-20 14:13:26 +0700

    tho13

--- src/pages/admin/product/addProduct/AddProduct.jsx
+++ src/pages/admin/product/addProduct/AddProduct.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { Input, Space, Form, Button } from 'antd';
-// import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Col, Row } from 'antd';
+import { Input, Space, Form, Button, Cascader } from 'antd';
+import { SwapRightOutlined } from '@ant-design/icons';
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import { AudioOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
@@ -50,19 +51,19 @@ function AddProduct() {
 
   const [loading, setLoading] = useState(false);
   const [imageUrl, setImageUrl] = useState();
-  const [dataFile , setDataFile] = useState(new FormData())
-
-  const listOrder = 'Chưa chọn ngành hàng'
+  const [branch, setBranch] = useState('')
+  const [categoryName, setCategoryName] = useState('')
+  const [idProduct, setIdProduct] = useState([])
+  const [dataFile, setDataFile] = useState(new FormData())
 
   const nav = useNavigate()
 
   const onFinish = async (values) => {
-    
+    console.log(72, 'Success:', values);
     try {
-      dataFile.append('productName' , values)
-      let res = await postAPI('/product/create-product', dataFile)
+      let res = await postAPI('/product/create-product', values)
       let idNewProduct = res.data.product._id
-      console.log('Success:', dataFile);
+      setIdProduct(idNewProduct)
       console.log(30, res);
       nav(`/admin/product/${idNewProduct}/detail/create`)
     } catch (error) {
@@ -79,55 +80,24 @@ function AddProduct() {
     console.log(24, 'Change:', e.target.value);
   };
 
-  function getItem(label, key, icon, children, type) {
-    return {
-      key,
-      icon,
-      children,
-      label,
-      type,
-    };
-  }
-
-  const items = [
-    getItem('Điện thoại', 'sub1', null, [
-      getItem(null, null, null,
-        [getItem('Apple', '1'),
-        getItem('SamSung', '2'),
-        getItem('Xiaomi', '3'),
-        getItem('OPPO', '4'),
-        getItem('Nokia', '6'),
-        getItem('ASUS', '8'),], 'group'),
-    ]),
-    getItem('Laptop', 'sub2', null, [
-
-    ]),
-    getItem('Máy tính bảng', 'sub3', null, [
-
-    ]),
-
-  ];
-
-  const onClick = (value) => {
-    console.log(88, 'click', value.key);
-    listOrder = value.key
-    return listOrder
-  };
-
-  console.log(listOrder);
-
-
-  const handleChange = (info) => {
-    console.log(118 , info);
+  const handleChange = async (info) => {
+    console.log(118, info);
     const formData = new FormData()
     setDataFile(formData)
-    formData.append('thumb' , info.file.originFileObj)
-
+    formData.append('thumb', info.file.originFileObj)
     getBase64(info.file.originFileObj, (url) => {
       setLoading(false);
       setImageUrl(url);
     });
   };
+
+  const handleChangeValue = (value) => {
+    console.log(96, value)
+    const categoryName = value[0]
+    const branchName = value[1]
+    setBranch(branchName)
+    setCategoryName(categoryName)
+  }
 
   const uploadButton = (
     <div>
@@ -142,9 +112,102 @@ function AddProduct() {
     </div>
   );
 
-
+  const options = [
+    {
+      value: 'Điện Thoại',
+      label: 'Điện Thoại',
+      children: [
+        {
+          name: 'branch',
+          value: 'Apple',
+          label: 'Apple',
+        },
+        {
+          value: 'SamSung',
+          label: 'SamSung',
+        },
+        {
+          value: 'Xiaomi',
+          label: 'Xiaomi',
+        },
+        {
+          value: 'OPPO',
+          label: 'OPPO',
+        },
+        {
+          value: 'Nokia',
+          label: 'Nokia',
+        },
+        {
+          value: 'ASUS',
+          label: 'ASUS',
+        },
+      ],
+    },
+    {
+      value: 'Máy tính bảng',
+      label: 'Máy tính bảng',
+      children: [
+        {
+          value: 'Ipad',
+          label: 'Ipad',
+        },
+        {
+          value: 'SamSung',
+          label: 'SamSung',
+        },
+        {
+          value: 'Nokia',
+          label: 'Nokia',
+        },
+        {
+          value: 'Xiaomi',
+          label: 'Xiaomi',
+        },
+        {
+          value: 'Lenovo',
+          label: 'Lenovo',
+        },
+      ],
+    },
+    {
+      value: 'Laptop',
+      label: 'Laptop',
+      children: [
+        {
+          value: 'Mac',
+          label: 'Mac',
+        },
+        {
+          value: 'Dell',
+          label: 'Dell',
+        },
+        {
+          value: 'ASUS',
+          label: 'ASUS',
+        },
+        {
+          value: 'Xiaomi',
+          label: 'Xiaomi',
+        },
+        {
+          value: 'Inrel',
+          label: 'Inrel',
+        },
+        {
+          value: 'HP',
+          label: 'HP',
+        },
+        {
+          value: 'Acer',
+          label: 'Acer',
+        },
+      ],
+    },
+  ]
   return (
     <div className='classAddProduct'>
+
       <div className='addProduct-header'>
         <h4>Thêm 1 sản phẩm mới</h4>
         <p>Vui lòng chọn ngành hàng phù hợp cho sản phẩm của bạn</p>
@@ -157,55 +220,57 @@ function AddProduct() {
           </Form.Item>
           <div className='addProduct-body'>
             <div className="input-search">
-              <Search
+              {/* <Search
                 placeholder="Tên ngành hàng"
                 onSearch={onSearch}
                 style={{
                   width: '30%',
                   borderRadius: 20
                 }}
-              /> <span>Chọn ngành hàng chính xác , <a className='link-addProduct' href="/">Bấm vào đây để tìm hiểu</a></span>
+              />  */}
+              <span>Chọn ngành hàng chính xác , <a className='link-addProduct' href="/">Bấm vào đây để tìm hiểu</a></span>
             </div>
-            <div className="menuAddProduct">
-              <Menu
-                onClick={onClick}
-                style={{
-                  width: 256,
-                }}
-                mode="vertical"
-                items={items}
+
+            <Form.Item label="Tên ngành hàng : " className='addBranch-addProduct' name='branch'>
+              <Cascader
+                onChange={handleChangeValue}
+                options={options}
               />
-            </div>
+            </Form.Item>
+
           </div>
 
           <div className='footer-addProduct'>
             <div className='chose-addProduct'>
-              Đã chọn :   <p> {listOrder}</p>
+              Đã chọn :   <p> {categoryName} <SwapRightOutlined /> {branch} </p>
             </div>
 
             <div className="upload-img">
               <h4>Thêm ảnh cho sản phẩm</h4>
               <div className='input-upload-img'>
-                <Upload
-                  name="avatar"
-                  listType="picture-card"
-                  className="avatar-uploader"
-                  showUploadList={false}
-                  beforeUpload={beforeUpload}
-                  onChange={handleChange}
-                >
-                  {imageUrl ? (
-                    <img
-                      src={imageUrl}
-                      alt="avatar"
-                      style={{
-                        width: '100%',
-                      }}
-                    />
-                  ) : (
-                    uploadButton
-                  )}
-                </Upload>
+
+                <Form.Item className='img-product' name="thumbnail" rules={[{ required: true, message: 'Hãy nhập vào ảnh của sản phẩm' }]} >
+                  <Upload
+                    name="avatar"
+                    listType="picture-card"
+                    className="avatar-uploader"
+                    showUploadList={false}
+                    beforeUpload={beforeUpload}
+                    onChange={handleChange}
+                  >
+                    {imageUrl ? (
+                      <img
+                        src={imageUrl}
+                        alt="avatar"
+                        style={{
+                          width: '100%',
+                        }}
+                      />
+                    ) : (
+                      uploadButton
+                    )}
+                  </Upload>
+                </Form.Item>
               </div>
             </div>
             <Form.Item wrapperCol={{ offset: 8, span: 14 }}>
@@ -222,3 +287,9 @@ function AddProduct() {
 }
 
 export default AddProduct
+
+
+
+
+
+
